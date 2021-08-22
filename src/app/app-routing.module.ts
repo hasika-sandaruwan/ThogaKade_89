710705908b4f048287652c05156dcf2e0c103685
seekerscloudlk <seekerscloudlkinfo@gmail.com>
2021-08-22 10:00:08 +0530

    5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {AuthenticationContextComponent} from "./core/authentication/authentication-context/authentication-context.component";
+import {LoginComponent} from "./core/authentication/login/login.component";
+import {SignUpComponent} from "./core/authentication/sign-up/sign-up.component";
 
 const routes: Routes = [
-  {path:'', redirectTo: '/auth', pathMatch:'full'},
-  {path:'auth', component: AuthenticationContextComponent}
+  {path: '', redirectTo: '/auth', pathMatch: 'full'},
+  {
+    path: 'auth', component: AuthenticationContextComponent, children: [
+      {path: '', redirectTo: '/auth/login', pathMatch: 'full'},
+      {path: 'login', component: LoginComponent},
+      {path: 'sign-up', component: SignUpComponent},
+    ]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
