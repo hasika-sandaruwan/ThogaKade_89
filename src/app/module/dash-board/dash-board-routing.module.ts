c8f4c53e66eac8a6880b2118ba01a790aab85995
seekerscloudlk <seekerscloudlkinfo@gmail.com>
2021-08-22 12:30:53 +0530

    15

--- src/app/module/dash-board/dash-board-routing.module.ts
+++ src/app/module/dash-board/dash-board-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashBoardComponent } from './dash-board.component';
 
-const routes: Routes = [{ path: '', component: DashBoardComponent }];
+const routes: Routes = [
+  { path: '', component: DashBoardComponent, children:[
+      { path: 'customer', loadChildren: () => import('./modules/customer/customer.module').then(m => m.CustomerModule) },
+      { path: 'item', loadChildren: () => import('./modules/item/item.module').then(m => m.ItemModule) },
+      { path: 'orders', loadChildren: () => import('./modules/orders/orders.module').then(m => m.OrdersModule) },
+      { path: 'place-order', loadChildren: () => import('./modules/place-order/place-order.module').then(m => m.PlaceOrderModule) }
+    ] },
+  ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
