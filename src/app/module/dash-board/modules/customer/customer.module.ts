38affa6fc7042ddc873dc30656d2e9a5871c1612
seekers-cloud-developers <developers.seekerscloud@gmail.com>
2021-08-29 13:43:48 +0530

    16

--- src/app/module/dash-board/modules/customer/customer.module.ts
+++ src/app/module/dash-board/modules/customer/customer.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
+import {MatTabsModule} from "@angular/material/tabs";
 
 
 @NgModule({
   declarations: [
     CustomerComponent
   ],
-  imports: [
-    CommonModule,
-    CustomerRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        CustomerRoutingModule,
+        MatTabsModule
+    ]
 })
 export class CustomerModule { }
