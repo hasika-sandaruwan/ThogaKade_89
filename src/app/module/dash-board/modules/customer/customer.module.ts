6d57d2ed73fe058b485b49327b070a7010533319
seekers-cloud-developers <developers.seekerscloud@gmail.com>
2021-08-29 13:54:28 +0530

    17

--- src/app/module/dash-board/modules/customer/customer.module.ts
+++ src/app/module/dash-board/modules/customer/customer.module.ts
@@ -4,11 +4,21 @@ import { CommonModule } from '@angular/common';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
 import {MatTabsModule} from "@angular/material/tabs";
+import { SaveCustomerComponent } from './components/save-customer/save-customer.component';
+import { UpdateCustomerComponent } from './components/update-customer/update-customer.component';
+import { DeleteCustomerComponent } from './components/delete-customer/delete-customer.component';
+import { SearchCustomerComponent } from './components/search-customer/search-customer.component';
+import { GetAllCustomersComponent } from './components/get-all-customers/get-all-customers.component';
 
 
 @NgModule({
   declarations: [
-    CustomerComponent
+    CustomerComponent,
+    SaveCustomerComponent,
+    UpdateCustomerComponent,
+    DeleteCustomerComponent,
+    SearchCustomerComponent,
+    GetAllCustomersComponent
   ],
     imports: [
         CommonModule,
