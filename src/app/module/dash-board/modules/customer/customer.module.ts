516bdc3232844f8b793197ab368ddd904b0853d3
seekers-cloud-developers <developers.seekerscloud@gmail.com>
2021-08-29 14:10:56 +0530

    18

--- src/app/module/dash-board/modules/customer/customer.module.ts
+++ src/app/module/dash-board/modules/customer/customer.module.ts
@@ -9,6 +9,10 @@ import { UpdateCustomerComponent } from './components/update-customer/update-cus
 import { DeleteCustomerComponent } from './components/delete-customer/delete-customer.component';
 import { SearchCustomerComponent } from './components/search-customer/search-customer.component';
 import { GetAllCustomersComponent } from './components/get-all-customers/get-all-customers.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatPaginatorModule} from "@angular/material/paginator";
 
 
 @NgModule({
@@ -20,10 +24,14 @@ import { GetAllCustomersComponent } from './components/get-all-customers/get-all
     SearchCustomerComponent,
     GetAllCustomersComponent
   ],
-    imports: [
-        CommonModule,
-        CustomerRoutingModule,
-        MatTabsModule
-    ]
+  imports: [
+    CommonModule,
+    CustomerRoutingModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatPaginatorModule
+  ]
 })
 export class CustomerModule { }
