c720c88ed4c1ea3974c7c8a920c95c20d481b869
seekers-cloud-developers <developers.seekerscloud@gmail.com>
2021-09-05 15:12:30 +0530

    25

--- src/app/module/dash-board/modules/place-order/place-order.component.ts
+++ src/app/module/dash-board/modules/place-order/place-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CustomerService} from "../../../../core/service/customer.service";
 
 @Component({
   selector: 'app-place-order',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceOrderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _customerService: CustomerService) { }
 
   ngOnInit(): void {
+    this.loadAllCustomers();
+  }
+  customers:any[]=[];
+  private loadAllCustomers() {
+    this._customerService.getAllCustomers().subscribe(response=>{
+      this.customers = response.data;
+    }, error => {
+      console.log(error);
+    })
   }
 
 }
